14b885c15dab4a0f140d17281dd38604439bdf20
Anastasia <Kirsanovasy@gmail.com>
2023-07-28 15:06:04 +0300

    New commit2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿onsole.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
